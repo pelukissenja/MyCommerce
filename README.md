e637e3479b64cd9832df0c5acbf9196a2df5ee4c
pelukissenja <hasan32@guru.smp.belajar.id>
2022-08-07 20:51:19 +0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# web-platform-zvzvd5
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/web-platform-zvzvd5)
---
+++ pages/script.js
@@ -0,0 +1,6 @@
+const link = document.querySelector('link');
+if (window.innerWidth < 1024) {
+  link.setAttribute('href', '../mobile.css');
+} else {
+  link.setAttribute('href', '../styles.css');
+}
